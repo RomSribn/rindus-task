a7df9949eb168763b07ac578f6846a9c43010e69
RomSribn <steeper.fest@gmail.com>
2023-06-29 20:44:53 +0200

    docs: add comments for the utils

--- src/utils/api.utils.ts
+++ src/utils/api.utils.ts
@@ -1,12 +1,26 @@
 import httpStatus from 'http-status';
 import { Response } from 'express';
 
+type TResponseData = {
+  result: any;
+  message: string;
+}
 interface APIResponseType {
   res: Response;
   data?: any;
 }
-
-export const APIResponse = ({ res, data = null }: APIResponseType) => {
+/**
+ * Response wrapper for controllers return data.
+ * @example 
+  return APIResponse({
+        res,
+        data: {
+          result: [],
+          message: 'Success',
+        },
+      });
+ */
+export const APIResponse = ({ res, data = null }: APIResponseType): { status: number; data: TResponseData } => {
   return res.status(httpStatus.OK).json({
     status: httpStatus.OK,
     data,
@@ -19,8 +33,17 @@ interface APIErrorType {
   message: string;
   error?: any;
 }
-
-export const APIError = ({ res, status, message, error = null }: APIErrorType) => {
+/**
+ * Response wrapper for controllers return error.
+ * @example 
+  throw APIError({
+        res,
+        status: httpStatus.INTERNAL_SERVER_ERROR,
+        message: 'Something went wrong',
+        error: err,
+      });
+ */
+export const APIError = ({ res, status, message, error = null }: APIErrorType): { status: number; message: string } => {
   if (error) console.error(error);
   return res.status(httpStatus.OK).json({ status, message });
 };
--- src/utils/transactions.utils.ts
+++ src/utils/transactions.utils.ts
@@ -1,6 +1,8 @@
 import mainDBConnection from '@config/db';
 import { Transaction } from 'sequelize';
-
+/**
+ * Wrapper with a database transaction
+ */
 export const executeTransaction = (callback: (t: Transaction) => PromiseLike<unknown>) => {
   return mainDBConnection.transaction(
     {
